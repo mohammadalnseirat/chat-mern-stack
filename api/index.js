2737f257229841e198f06416d14d7c67f00a0ca1
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-21 19:04:08 +0400

    Add MiddleWare and Create Function To HandleErrors

--- api/index.js
+++ api/index.js
@@ -1,13 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectionToDatabase } from "./config/db.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(express.json());
+app.use(express.json()); //! to parse the incoming requests with JSON payloads (from req.body)
+
+// ?Routes here:
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
   connectionToDatabase();
   console.log(`server is running on port ${PORT}`);
 });
+
+// *Middleware to Handle Errors:
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode
+  })
+})
