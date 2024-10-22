d0db679984ee470c3103d8f47a97d09bdcfe0b7c
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-22 19:56:45 +0400

    Create Send Message Api Route

--- api/controllers/message.controller.js
+++ api/controllers/message.controller.js
@@ -5,8 +5,53 @@ import { handleErrors } from "../utils/error.js";
 // !Function to send message:
 export const sendMessage_Post = async (req, res, next) => {
   try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+    // ?Find The Conversation:
+    let conversation = await Conversation.findOne({
+      participants: {
+        $all: [senderId, receiverId],
+      },
+    });
+    if (!conversation) {
+      // ?Create New Conversation:
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    // ?Create New Message:
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    // *Push the new message in the conversation:
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+
+    // ?save the conversation and message:
+    // !This way to save will take time to save in data base instaed of we will use promises:
+    // await conversation.save();
+    // await newMessage.save();
+    //! this will run in parallel:
+    await Promise.all([conversation.save(), newMessage.save()]);
+    // ?Send the response back:
+    res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error while sending message", error.message);
+    next(error);
+  }
+};
+
+// !Function to get messages:
+export const getMessages_Get = async (req, res, next) => {
+  try {
+  } catch (error) {
+    console.log("Error while getting messages", error.message);
     next(error);
   }
 };
--- api/routes/message.route.js
+++ api/routes/message.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { sendMessage_Post } from "../controllers/message.controller.js";
+import {
+  getMessages_Get,
+  sendMessage_Post,
+} from "../controllers/message.controller.js";
 import protectRoute from "../lib/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/send-message", protectRoute, sendMessage_Post);
+router.get("/:id", protectRoute, getMessages_Get);
+router.post("/send-message/:id", protectRoute, sendMessage_Post);
 
 export default router;
